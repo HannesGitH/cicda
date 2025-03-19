c8d4e79d33e2f8a99a7feb5f8de1276952c9562f
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-03-20 00:30:35 +0100

    compat

--- src/lib/features/beam/index.ts
+++ src/lib/features/beam/index.ts
@@ -1,7 +1,6 @@
-import { octo_app } from "$lib/common/octo.server";
-import { version } from "$service-worker";
+import { getOctoApp } from "$lib/common/octo.server";
 import type { Octokit } from "octokit";
-import { execSync } from "child_process";
+import { execSync } from 'node:child_process';
 
 type Version = `${number}.${number}.${number}`;
 
