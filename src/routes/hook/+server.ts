50e4a83022ea0f092222e887249907ed8f694b7f
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-03-21 14:51:59 +0100

    catch and ret resp

--- src/routes/hook/+server.ts
+++ src/routes/hook/+server.ts
@@ -2,16 +2,23 @@ import { getOctoApp } from "$lib/common/octo.server";
 import { onPullRequest } from "$lib/hooks";
 
 
-export async function POST({ request }) : Promise<void> {
+export async function POST({ request })  {
 
     const octo_app = getOctoApp();
 
     octo_app.webhooks.on("pull_request", onPullRequest);
 
-	octo_app.webhooks.verifyAndReceive({
-		id: request.headers.get("x-github-delivery")!,
-		name: request.headers.get("x-github-event")!,
-        signature: request.headers.get("x-hub-signature-256")!,
-		payload: await request.json(),
-	});
+	try {
+		await octo_app.webhooks.verifyAndReceive({
+			id: request.headers.get("x-github-delivery")!,
+			name: request.headers.get("x-github-event")!,
+			signature: request.headers.get("x-hub-signature-256")!,
+			payload: await request.json(),
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response("Error", { status: 400 });
+	}
+
+	return new Response("OK");
 }
